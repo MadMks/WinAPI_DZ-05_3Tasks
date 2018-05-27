e819a7d78f77ef3b4837362b5f3dec0a65734f27
MadMks <mks59k@gmail.com>
2018-05-27 16:18:27 +0300

    Работает создание элемента.

--- Task_3_Controls/Source.cpp
+++ Task_3_Controls/Source.cpp
@@ -3,7 +3,8 @@
 #include "resource.h"
 using namespace std;
 
-
+HWND hComboBox;
+HINSTANCE hInstance;
 
 BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
 
@@ -15,6 +16,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpszCmdLine, int nCmd
 	HWND hDialog = CreateDialog(hInst, MAKEINTRESOURCE(IDD_DIALOG_Main), NULL, DlgProc);
 	ShowWindow(hDialog, nCmdShow);
 
+	hInstance = hInst;
 
 	while (GetMessage(&msg, 0, 0, 0))
 	{
@@ -28,7 +30,11 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpszCmdLine, int nCmd
 
 BOOL CALLBACK DlgProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
 {
-
+	TCHAR szElements[5][10] =
+	{
+		L"BUTTON", L"EDIT", L"LISTBOX", L"COMBOBOX", L"STATIC"
+	};
+	TCHAR szSelectElement[10];
 
 	switch (uMessage)
 	{
@@ -39,7 +45,13 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
 
 	case WM_INITDIALOG:
 
+		hComboBox = GetDlgItem(hWnd, IDC_COMBO);
 
+		for (int i = 0; i < 5; i++)
+		{
+			SendMessage(hComboBox, CB_ADDSTRING, (WPARAM)0, (LPARAM)szElements[i]);
+		}
+		SendMessage(hComboBox, CB_SETCURSEL, (WPARAM)0, (LPARAM)0);
 
 		return TRUE;
 
@@ -47,11 +59,22 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
 
 		if (LOWORD(wParam) == IDC_BUTTON_Create)
 		{
-			switch (switch_on)
+			int iNumberElement = SendMessage(hComboBox, CB_GETCURSEL, (WPARAM)0, (LPARAM)0);
+
+			switch (iNumberElement)
 			{
+			case 0:
+
+				break;
 			default:
 				break;
 			}
+
+			/*hEdit1 = */CreateWindowEx(WS_EX_CLIENTEDGE,
+				szElements[iNumberElement], 0, WS_CHILD | WS_VISIBLE |
+				WS_VSCROLL | WS_TABSTOP | ES_WANTRETURN |
+				ES_MULTILINE | ES_AUTOVSCROLL, 10, 7, 150,
+				100, hWnd, 0, hInstance, 0);
 		}
 		else if (LOWORD(wParam) == IDC_BUTTON_Delete)
 		{
